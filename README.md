48dea05bc74dca9d8df3ec1a52a5b7671bd2e1c8
nikita uglanitsa <uglanican8@gmail.com>
2023-04-14 14:59:25 +0300

    Version 1.1

--- README.md
+++ README.md
@@ -6,12 +6,21 @@ This repository contains files to create a web application for predicting surviv
 * requirements.txt: requirements for your virtual environment.
 * titanic_model.ipynb: Jupyter research file on Titanic data and how the model works.
 * titanic_model.joblib: model from research file.
+## Updates 
+### Version 1.1
+Optimized the program: We made some changes to optimize the performance of the program.
+* Improved model accuracy by applying standardization to the input features.
+* Added a new fare determination system based on the distance between the embarkation port and the destination port.
+* We trained the model on all the available data to improve the accuracy of the model.
+* Added comments to the code to improve its readability and maintainability.
+### Version 1.0
+* Created the project with a logistic regression model to predict survival on the Titanic ship.
 ## How to use
 1. Clone the repository to your computer.
 2. Open a terminal and navigate to the repository folder.
 3. Install the required dependencies by running pip install -r requirements.txt.
 4. Run the main.py script in the terminal and launch the web application using the command: python -m streamlit run main.py.
-5. Enter values for each of the parameters and press Enter.
+5. Enter values for each of the parameters and press Predict button.
 6. The script will output a prediction on whether or not the passenger survived on the ship.
 ## Example
 You can try this app by following this link: https://nicksttar-titanic-survival-prediction.streamlit.app/
--- main.py
+++ main.py
@@ -1,48 +1,108 @@
-import streamlit as st
 import numpy as np
+import pandas as pd
+import streamlit as st
 from joblib import load
+from sklearn.preprocessing import StandardScaler
 
-st.title('Can you survive Titanic?')
-st.markdown('_Imagine that you have boarded the Titanic in 1912, what would have been your fate?\
-            This test allows for an 80% accuracy in determining whether you would have survived or not. \
-            The data used to train this model was sourced from Kaggle. \
-            Make by nicksttar_')
-
-model = load('titanic_model.joblib')
-
+st.title('Titanic Survival Prediction')
+st.markdown('_Imagine that you have boarded the Titanic in 1912, what would have been your fate? '
+            'This test allows for an 80% accuracy in determining whether you would have survived or not. '
+            'The data used to train this model was sourced from Kaggle. '
+            'Made by nicksttar._')
 
 
-def predict_survival(pclass, age, sibsp, parch, fare, sex_male, q, s):
-    data = np.array([[pclass, age, sibsp, parch, fare, sex_male, q, s]])
-    prediction =model.predict(data)
+def predict_survival(pclass: int, age: int, sibsp: str, parch: str, fare: int, sex_male: str, q: int, s: int) -> str:
+    """
+    This function takes in various features related to a passenger on the Titanic and predicts 
+    whether they would survive or not based on the given model. The features include the passenger's 
+    cabin class, age, number of siblings/spouses aboard, number of parents/children aboard, fare, 
+    gender, and the port of embarkation. The function returns a string with the predicted outcome 
+    and the probability of survival in percentage.
+
+    Args:
+    - pclass: int, the cabin class of the passenger (1, 2, or 3).
+    - age: int, the age of the passenger.
+    - sibsp: str, 'Yes' or 'No', whether the passenger has parents aboard.
+    - parch: str, 'Yes' or 'No', whether the passenger has wife/husband aboard.
+    - fare: float, the fare paid by the passenger.
+    - sex_male: str, 'male' or 'female', the gender of the passenger.
+    - q: int, binary variable indicating whether the passenger embarked from Queenstown.
+    - s: int, binary variable indicating whether the passenger embarked from Southampton.
+
+    Returns:
+    - A string with the predicted outcome and the probability of survival in percentage.
+    """
+    # Load the scaler and data used to scale the features
+    model = load('base_models/titanic_model.joblib')
+
+    # Scaling
+    X_need_to_scale = pd.read_csv('base_models/X_need_to_scale.csv')
+    scaler = StandardScaler()
+    scaler.fit(X_need_to_scale)
+
+    # Take data and transform it
+    data = [[pclass, age, sibsp, parch, fare, sex_male, q, s]]
+    data = scaler.transform(data)
+
+    # Make prediction and probabillity
+    prediction = model.predict(data)
     proba = model.predict_proba(data)
     proba_a = str(proba[0][1] * 100)[:4]
     proba_na = str(proba[0][0] * 100)[:4]
     if prediction[0] == 0:
-        return f'Ohhh, you would die in titanic if you was there with chance of {proba_na}%'
+        return f'Ohhh, you would die in Titanic if you were there with chance of {proba_na}%'
     else:
-        return f'Congratulation you are alive with chance of {proba_a}% '
+        return f'Congratulations, you are alive with chance of {proba_a}%'
+
+def string_to_digit(sibsp: str, parch: str, sex_male: str, port: str) -> list:
+    """
+    This function takes in four string arguments representing various features related to a passenger on the Titanic. 
+    Sibsp, parch, sex_male, and port. It converts these features to numeric values and returns a list containing these values.
+    """
+    sibsp = int(sibsp == 'Yes')
+    parch = int(parch == 'Yes')
+    sex_male = int(sex_male == 'male')
+    port_map = {'Cherbourg': (0, 0), 'Queenstown': (1, 0), 'Southampton': (0, 1)}
+    q, s = port_map.get(port, (0, 0))
+    return [sibsp, parch, sex_male, q, s]
+
+def predict_fare(pclass: int, age: int, sibsp: str, parch: str, sex_male: str, port: str) -> float:
+    # Convert categorical variables to numerical
+    convert = string_to_digit(sibsp, parch, sex_male, port)
     
+    # Load data and model
+    X = pd.read_csv('base_models/X_need_to_fare_scale.csv')
+    model = load('base_models/fare_model.joblib')
+    
+    # Scale the data
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    
+    # Prepare the data for prediction
+    data = [[pclass, age, convert[0], convert[1], convert[2], convert[-2], convert[-1]]]
+    data = scaler.transform(data)
+
+    # Make the prediction
+    prediction = model.predict(data)
+    fare = prediction[0]
+
+    # Adjust fare for first-class passengers
+    if pclass == 1:
+        fare += 30  # From tests we have seen that the average fare for first-class passengers is 30 higher than the predicted fare
+
+    return float(fare)
+    
+
 pclass = st.selectbox('Cabin class: ', [1, 2, 3])
 age = st.slider('Age: ', 0, 100, step=1)
 sibsp = st.selectbox('Have you parents on board: ', ['Yes', 'No'])
-parch = st.selectbox('Have you girlfrien/boyfried on board: ', ['Yes', 'No'])
+parch = st.selectbox('Have you girlfriend/boyfriend on board: ', ['Yes', 'No'])
 sex_male = st.selectbox('Gender: ', ['male', 'female'])
 port = st.selectbox('Port of your boarding', ['Cherbourg', 'Queenstown', 'Southampton'])
 
 if st.button("Predict"):
-    fare = 32 # Mean of column Fare from data
-    sibsp = (0, 1)[sibsp == 'Yes']
-    parch = (0, 1)[parch == 'Yes']
-    sex_male = (0, 1)[sex_male == 'male']
-    if port == 'Cherbourg': 
-        q = 0
-        s = 0
-    elif port == 'Queenstown':
-        q = 1
-        s = 0
-    elif port == 'Southampton':
-        q = 0
-        s = 1
-    result = predict_survival(pclass, age, sibsp, parch, fare, sex_male, q, s)
+    fare = predict_fare(pclass, age, sibsp, parch, sex_male, port)
+    convert = string_to_digit(sibsp, parch, sex_male, port)
+    result = predict_survival(pclass, age, convert[0], convert[1], fare, convert[2], convert[-2], convert[-1])
     st.write(result)
+    
